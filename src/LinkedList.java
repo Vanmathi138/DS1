6f96e7522049b02b28401ed7206f90274a0b4742
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2025-05-27 11:45:50 +0530

    use case1 for create linked list

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -9,8 +9,24 @@ class Node{
 }
 public class LinkedList {
     public static void main(String[] args) {
-        Node head = new Node(56);
-        head.next = new Node(30);
-        head.next.next = new Node(70);
+        Node head = new Node(70);
+
+        //add 30
+        Node newNode1 = new Node(30);
+        newNode1.next = head;
+        head = newNode1;
+
+        //add 56
+        Node newNode2 = new Node(56);
+        newNode2.next = head;
+        head = newNode2;
+
+
+        while (head!=null){
+            System.out.print(head.data+" ");
+            head = head.next;
+        }
+
+
     }
 }
