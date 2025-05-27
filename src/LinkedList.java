fb7fa2547a6d08325394cbc4521cdc6b30deb4ea
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2025-05-27 12:07:47 +0530

    use case 4 for insert an element into linked list

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -10,14 +10,17 @@ class Node{
 public class LinkedList {
     public static void main(String[] args) {
         Node head = new Node(56);
-        head.next = new Node(30);
-        head.next.next = new Node(70);
+        head.next = new Node(70);
+
+
+        Node newNode = new Node(30);
+        newNode.next = head.next;
+        head.next = newNode;
 
         while (head!=null){
             System.out.print(head.data+" ");
             head = head.next;
         }
-
 
     }
 }
